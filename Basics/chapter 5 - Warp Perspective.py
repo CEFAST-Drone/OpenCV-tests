8dcab33d7da894dd67bd6f3025d766a408939da3
Jocker00 <grove1411@gmail.com>
2022-03-17 18:09:26 -0300

    Chapter 5 - Warp Perspective

---
+++ Basics/chapter 5 - Warp Perspective.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("test-resources\street.jpg")
+
+# Letting the "levis" advertise plan and easy to read
+width, height = 200, 200 # sizes of the outdoor
+point1 = np.float32([[61,0],[100,22],[61,95],[100,108]]) # Position of the outdoor (the 4 points of it)
+point2 = np.float32([[0,0],[width, 0],[0, height],[width, height]]) # New sizes
+matrix = cv2.getPerspectiveTransform(point1, point2) # Make it plan
+
+imgOutput = cv2.warpPerspective(img, matrix, (width, height)) # Turn it into an image
+
+cv2.imshow("Original Image", img)
+cv2.imshow("Levis outdoor", imgOutput)
+
+cv2.waitKey(0)
