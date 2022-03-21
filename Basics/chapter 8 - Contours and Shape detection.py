6ced086e845a7743d131aab26f3cff1033f84a8f
Jocker00 <grove1411@gmail.com>
2022-03-21 18:24:49 -0300

    Chapter 8 - Object Detection

---
+++ Basics/chapter 8 - Contours and Shape detection.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+from functions import stack
+
+# Contour detection Function
+def ContourDetection(img, originalImg):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+    # (image, mode, method), mode have a lot of options, method it's the way you want the return data to be.
+
+    imgContour = originalImg.copy() # Save the results
+
+    for count in contours:
+        area = cv2.contourArea(count)
+        print(area)
+        if area > 50: # Avoid errors
+            cv2.drawContours(imgContour, count, -1, (0, 255, 0), 3) 
+            # (img to put contour, border, -1 is to display all of the contour, color of the border, thickness)
+            perimeter = cv2.arcLength(count, True) # (border, True)
+            print(perimeter)
+            approx = cv2.approxPolyDP(count, 0.02*perimeter, True) # (border, I don't know, True)
+            vertices = len(approx)
+            print(vertices)
+            x, y, w, h = cv2.boundingRect(approx) # Get the position of the Object, with x, y, width and height
+            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (255, 255, 0), 2)
+            # We can save that information to calculate the object's position
+
+            # Putting the name of the object in it
+            if vertices == 3: objectType = "Triangle"
+            elif vertices == 4: objectType = "Rectangle"
+            elif vertices == 8: objectType = "Circle"
+            else: objectType = "?"
+            cv2.putText(imgContour, objectType, (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.2, (0,0,0))
+    
+    return imgContour
+
+img = cv2.imread("test-resources/shapes.png")
+
+# Contours detection ("bordas") 
+
+# First, filtering the image to get easier to detect Borders
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Black and white image
+imgBlur = cv2.GaussianBlur(imgGray, (5,5), 1) # Blur image -> Easier and faster to use
+imgCanny = cv2.Canny(img, 100, 100) # Contour detection
+
+imgResult = ContourDetection(imgCanny, img)
+
+#cv2.imshow("Original Image", img)
+#cv2.imshow("Sent Image", imgCanny)
+#cv2.imshow("Final Image", imgResult)
+
+imgStack = stack.concat_tile_resize([[img, imgResult]])
+cv2.imshow("Stack Images", imgStack)
+
+cv2.waitKey(0)
--- Basics/functions/stack.py
+++ Basics/functions/stack.py
@@ -1,7 +1,4 @@
-# This hero's code (stack functions): https://github.com/nkmk/python-snippets/blob/e5d988b84b04acfbfd15105546c93c2465f2d126/notebook/opencv_hconcat_vconcat_np_tile.py#L57-L64
-
 import cv2
-import numpy as np
 
 def vconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
     w_min = min(im.shape[1] for im in im_list)
@@ -22,19 +19,7 @@ def concat_tile_resize(im_list_2d, interpolation=cv2.INTER_CUBIC):
     im_list_v = [hconcat_resize_min(im_list_h, interpolation=cv2.INTER_CUBIC) for im_list_h in im_list_2d]
     return vconcat_resize_min(im_list_v, interpolation=cv2.INTER_CUBIC)
 
-img = cv2.imread("test-resources/street.jpg")
-
-im_tile_resize = concat_tile_resize([[img],
-                                     [img, img, img, img, img],
-                                     [img, img, img]])
-
-img = cv2.resize(img, (373, 199)) # Resizing to get smaller image
-
-imgHor = np.hstack((img, img, img)) # Horizontally stack
-imgVer = np.vstack((img, img)) # Horizontally stack
-
-cv2.imshow("Horizontal", imgHor)
-cv2.imshow("Vertical", imgVer)
-cv2.imshow("Stack", im_tile_resize)
-
-cv2.waitKey(0)
+# To use: 
+# from functions import stack
+# StackImg = stack.concat_tile_resize([[img],
+#                                       [img, img]])    
